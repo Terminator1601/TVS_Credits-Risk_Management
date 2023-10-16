758c287bfcb97c59a5d8b403e893252af4d81700
Terminator1601 <rishisharma4201@gmail.com>
2023-10-17 02:01:08 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+import sqlite3
+import os
+
+current_Directory = os.path.dirname(os.path.abspath(__file__))
+print(current_Directory)
+
+model = pickle.load(open('trained_model1_rf.sav', 'rb'))
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def man():
+    return render_template('home.html')
+
+
+@app.route('/predict', methods=['POST'])
+def home():
+    # return max_loan_id + 1 if max_loan_id is not None else 1
+
+    # loan_id = max_loan_id + 1 if max_loan_id is not None else 1
+    applicant_name = request.form.get('Name')
+    Gender = request.form.get('Gender')
+    Marital_status = request.form.get('Married')
+    Dependents = request.form.get('Dependents')
+    Education = request.form.get('Education')
+    Property = request.form.get('Property_Area')
+    Employment = request.form.get('Employment')
+    Loan_Term = request.form.get('Loan_Amount_Term')
+    applicant_income = float(request.form.get('ApplicantIncome'))
+    coapplicant_income = float(request.form.get('CoapplicantIncome'))
+    loan_amount = float(request.form.get('LoanAmount'))
+    credit_history = float(request.form.get('Credit_History'))
+
+    # Prepare the input data for prediction
+    input_data = np.array([Gender, Marital_status, Dependents, Education, Employment, applicant_income, coapplicant_income,
+                          loan_amount,Loan_Term, credit_history, Property]).reshape(1, -1)
+    # input_data_reshape = input_data_numpy.reshape(1, -1)
+    prediction = model.predict(input_data)
+    # percentage_approval = (X_test_prediction == 'Y').mean() * 100
+
+    if prediction[0] == 0:
+        status = 'n'
+    else:
+        status = 'y' 
+
+    # making connection with database
+
+    connection = sqlite3.connect(
+        current_Directory + "\\static\\database\\tvs_credit")
+    cursor = connection.cursor()
+    cursor.execute('SELECT MAX(loan_id) FROM userData')
+    max_loan_id = cursor.fetchone()[0]
+    # connection.close()
+    loan_id = max_loan_id + 1 if max_loan_id is not None else 1
+    cursor = connection.cursor()
+    query1 = "INSERT INTO userData (loan_id, name, gender, married_status, dependent, education, employment, property_area, a_income, co_income, loan_amount,loan_amt_term, credit_history,loan_status) VALUES (?,?, ?, ?, ?, ?, ?, ?,?, ?,?, ?, ?, ?)"
+    cursor.execute(query1, (loan_id, applicant_name, Gender, Marital_status, Dependents, Education,
+                   Employment, Property, applicant_income, coapplicant_income, loan_amount, Loan_Term, credit_history, status))
+
+    # query1="INSERT INTO userData VALUES({a_name},{gen}.{married},'{dependent}','{edu}','{emp}','{p_area}','{a_income}','{co_income}','{l_amt}','{c_score}')".format(a_name=applicant_name,gen=Gender,married=Marital_status,dependent=Dependents,edu=Education,emp=Employment,p_area=Property,a_income=applicant_income,co_income=coapplicant_income,l_amt=loan_amount,c_score=credit_history)
+    # cursor.execute(query1)
+    connection.commit()
+    print(loan_amount,loan_id)
+
+    # print('Percentage of loan approvals in the test data:', percentage_approvall  )
+  
+    if prediction[0] == '0':
+        result = 'Loan is not approved'
+    else:
+        result = 'Loan is approved'
+
+    return render_template('after.html', prediction_text=result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ iris.py
@@ -0,0 +1,136 @@
+
+
+# import numpy as np
+# import pandas as pd
+# import seaborn as sns
+# from sklearn.model_selection import train_test_split
+# from sklearn import svm
+# from sklearn.metrics import accuracy_score
+# import pickle
+
+# loan_dataset = pd.read_csv('./demo1_data.csv')
+
+# loan_dataset = loan_dataset.dropna()
+
+# loan_dataset = loan_dataset.replace(to_replace="N", value="0")
+# loan_dataset = loan_dataset.replace(to_replace="Y", value="1")
+# loan_dataset = loan_dataset.replace(to_replace="Yes", value="1")
+# loan_dataset = loan_dataset.replace(to_replace="No", value="0")
+# loan_dataset = loan_dataset.replace(to_replace="Male", value="1")
+# loan_dataset = loan_dataset.replace(to_replace="Female", value="0")
+# loan_dataset = loan_dataset.replace(to_replace="Graduate", value="1")
+# loan_dataset = loan_dataset.replace(to_replace="Not Graduate", value="0")
+# loan_dataset = loan_dataset.replace(to_replace="Rural", value="0")
+# loan_dataset = loan_dataset.replace(to_replace="Urban", value="2")
+# loan_dataset = loan_dataset.replace(to_replace="Semiurban", value="1")
+# loan_dataset = loan_dataset.replace(to_replace="3+", value="4")
+
+# X = loan_dataset.drop(columns=['Loan_ID', 'Loan_Status'], axis=1)
+# Y = loan_dataset['Loan_Status']
+
+# X_train, X_test, Y_train, Y_test = train_test_split(
+#     X, Y, test_size=0.1, stratify=Y, random_state=2)
+
+# classifier = svm.SVC(kernel='linear')
+# classifier.fit(X_train, Y_train)
+
+# X_train_prediction = classifier.predict(X_train)
+# training_data_accuray = accuracy_score(X_train_prediction, Y_train)
+
+# X_test_prediction = classifier.predict(X_test)
+# test_data_accuray = accuracy_score(X_test_prediction, Y_test)
+# percentage_approval = (X_test_prediction == 'Y').mean() * 100
+
+# print('Percentage of loan approvals in the test data:', percentage_approval)
+
+# filename = 'trained_model.sav'
+# pickle.dump(classifier, open(filename, 'wb'))
+
+# loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+
+# input_data = (1, 0, 1, 0, 1, 4053, 2026, 158, 560, 0, 2)
+# input_data_numpy = np.asarray(input_data)
+# input_data_reshape = input_data_numpy.reshape(1, -1)
+# prediction = classifier.predict(input_data_reshape)
+
+# print('Accuracy on training data : ', training_data_accuray)
+# print('Accuracy on test data : ', test_data_accuray)
+
+
+# print(prediction)
+# if (prediction[0] == '0'):
+#     print('loan is not approved')
+# else:
+#     print('loan is approved')
+
+
+
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import pickle
+
+# Load the dataset
+loan_dataset = pd.read_csv('./demo1_data.csv')
+
+# Feature Engineering and Data Preprocessing
+
+
+loan_dataset = loan_dataset.dropna()
+
+loan_dataset = loan_dataset.replace(to_replace="N", value="0")
+loan_dataset = loan_dataset.replace(to_replace="Y", value="1")
+loan_dataset = loan_dataset.replace(to_replace="Yes", value="1")
+loan_dataset = loan_dataset.replace(to_replace="No", value="0")
+loan_dataset = loan_dataset.replace(to_replace="Male", value="1")
+loan_dataset = loan_dataset.replace(to_replace="Female", value="0")
+loan_dataset = loan_dataset.replace(to_replace="Graduate", value="1")
+loan_dataset = loan_dataset.replace(to_replace="Not Graduate", value="0")
+loan_dataset = loan_dataset.replace(to_replace="Rural", value="0")
+loan_dataset = loan_dataset.replace(to_replace="Urban", value="2")
+loan_dataset = loan_dataset.replace(to_replace="Semiurban", value="1")
+loan_dataset = loan_dataset.replace(to_replace="3+", value="4")
+
+
+# ... (Your preprocessing code here)
+
+# Separate features and target variable
+X = loan_dataset.drop(columns=['Loan_ID', 'Loan_Status'], axis=1)
+Y = loan_dataset['Loan_Status']
+
+# Split the data into training and testing sets
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, stratify=Y, random_state=2)
+
+# Model Selection and Hyperparameter Tuning (Random Forest)
+param_grid = {
+    'n_estimators': [100, 200, 300],
+    'max_depth': [5, 10, 15],
+    'min_samples_split': [2, 5, 10]
+}
+
+rf_classifier = RandomForestClassifier(random_state=2)
+grid_search = GridSearchCV(rf_classifier, param_grid, cv=5, n_jobs=-1)
+grid_search.fit(X_train, Y_train)
+
+best_rf_classifier = grid_search.best_estimator_
+
+# Evaluate the model
+X_train_prediction = best_rf_classifier.predict(X_train)
+training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+
+X_test_prediction = best_rf_classifier.predict(X_test)
+test_data_accuracy = accuracy_score(X_test_prediction, Y_test)
+percentage_approval = (X_test_prediction == 'Y').mean() * 100
+
+# print('Percentage of loan approvals in the test data:', percentage_approval)
+
+# Save the trained model
+filename = 'trained_model1_rf.sav'
+pickle.dump(best_rf_classifier, open(filename, 'wb'))
+
+
+print('Accuracy on training data : ', training_data_accuracy)
+print('Accuracy on test data : ', test_data_accuracy)
