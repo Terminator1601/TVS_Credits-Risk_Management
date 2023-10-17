655de05aecae51f7a86807bd40df8656af749026
Terminator1601 <rishisharma4201@gmail.com>
2023-10-17 15:22:06 +0530

    added variables in the solution page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import pickle
 import numpy as np
 import sqlite3
@@ -37,7 +37,7 @@ def home():
 
     # Prepare the input data for prediction
     input_data = np.array([Gender, Marital_status, Dependents, Education, Employment, applicant_income, coapplicant_income,
-                          loan_amount,Loan_Term, credit_history, Property]).reshape(1, -1)
+                          loan_amount, Loan_Term, credit_history, Property]).reshape(1, -1)
     # input_data_reshape = input_data_numpy.reshape(1, -1)
     prediction = model.predict(input_data)
     # percentage_approval = (X_test_prediction == 'Y').mean() * 100
@@ -45,7 +45,7 @@ def home():
     if prediction[0] == 0:
         status = 'n'
     else:
-        status = 'y' 
+        status = 'y'
 
     # making connection with database
 
@@ -64,16 +64,35 @@ def home():
     # query1="INSERT INTO userData VALUES({a_name},{gen}.{married},'{dependent}','{edu}','{emp}','{p_area}','{a_income}','{co_income}','{l_amt}','{c_score}')".format(a_name=applicant_name,gen=Gender,married=Marital_status,dependent=Dependents,edu=Education,emp=Employment,p_area=Property,a_income=applicant_income,co_income=coapplicant_income,l_amt=loan_amount,c_score=credit_history)
     # cursor.execute(query1)
     connection.commit()
-    print(loan_amount,loan_id)
+    print(loan_amount, loan_id)
+
+    return redirect(url_for('solutions', result=result, credit_history=credit_history, Loan_Term=Loan_Term, coapplicant_income=coapplicant_income))
 
     # print('Percentage of loan approvals in the test data:', percentage_approvall  )
-  
+
     if prediction[0] == '0':
-        result = 'Loan is not approved'
+        result = 'rejected'
     else:
-        result = 'Loan is approved'
+        result = 'approved'
+    print(result)
+    data_to_pass = {
+        'credit_history': credit_history,
+        'Loan_Term': Loan_Term,
+        'coapplicant_income': coapplicant_income
+    }
 
     return render_template('after.html', prediction_text=result)
+
+
+@app.route('/solution')
+def solution():
+    result = request.args.get('result')
+    credit_history = request.args.get('credit_history')
+    Loan_Term = request.args.get('Loan_Term')
+    coapplicant_income = request.args.get('coapplicant_income')
+
+    # Render the 'alternative.html' template and pass the data for display
+    return render_template('alternative.html', result=result, credit_history=credit_history, Loan_Term=Loan_Term, coapplicant_income=coapplicant_income)
 
 
 if __name__ == "__main__":
